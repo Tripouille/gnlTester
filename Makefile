246e84cf3c13e117975a006e5339d67be325f79a
jgambard <jmichel.gambard@gmail.com>
2020-12-01 07:18:20 +0100

    add check still reachable

--- Makefile
+++ Makefile
@@ -28,19 +28,19 @@ $(1MANDATORY_OBJS): 1%.o: %.c $(MANDATORY_HEADER)
 	@gcc -D BUFFER_SIZE=1 $(CFLAGS) -c $*.c -o $*.o
 
 $(1MANDATORY): 1%: mandatory_start $(1MANDATORY_OBJS)
-	clang++ -D BUFFER_SIZE=1 $(CPPFLAGS) $(UTILS) $(TESTS_PATH)$*.cpp $(MANDATORY_OBJS) && valgrind -q ./a.out && rm -f a.out
+	clang++ -D BUFFER_SIZE=1 $(CPPFLAGS) $(UTILS) $(TESTS_PATH)$*.cpp $(MANDATORY_OBJS) && valgrind -q --leak-check=full --show-reachable=yes ./a.out && rm -f a.out
 
 $(42MANDATORY_OBJS): 42%.o: %.c $(MANDATORY_HEADER)
 	@gcc -D BUFFER_SIZE=42 $(CFLAGS) -c $*.c -o $*.o
 
 $(42MANDATORY): 42%: mandatory_start $(42MANDATORY_OBJS)
-	@clang++ -D BUFFER_SIZE=42 $(CPPFLAGS) $(UTILS) $(TESTS_PATH)$*.cpp $(MANDATORY_OBJS) && valgrind -q ./a.out && rm -f a.out
+	@clang++ -D BUFFER_SIZE=42 $(CPPFLAGS) $(UTILS) $(TESTS_PATH)$*.cpp $(MANDATORY_OBJS) && valgrind -q --leak-check=full --show-reachable=yes ./a.out && rm -f a.out
 
 $(10000000MANDATORY_OBJS): 10000000%.o: %.c $(MANDATORY_HEADER)
 	@gcc -D BUFFER_SIZE=10000000 $(CFLAGS) -c $*.c -o $*.o
 
 $(10000000MANDATORY): 10000000%: mandatory_start $(10000000MANDATORY_OBJS)
-	@clang++ -D BUFFER_SIZE=10000000 $(CPPFLAGS) $(UTILS) $(TESTS_PATH)$*.cpp $(MANDATORY_OBJS) && valgrind -q ./a.out && rm -f a.out
+	@clang++ -D BUFFER_SIZE=10000000 $(CPPFLAGS) $(UTILS) $(TESTS_PATH)$*.cpp $(MANDATORY_OBJS) && valgrind -q --leak-check=full --show-reachable=yes ./a.out && rm -f a.out
 
 $(VMANDATORY): v%: mandatory_start
 
--- tests/Files.cpp
+++ tests/Files.cpp
@@ -13,6 +13,14 @@ extern "C"
 #include <fcntl.h>
 #include <unistd.h>
 
+void readAll(int fd)
+{
+	char * s;
+	while (get_next_line(fd, &s) > 0)
+		free(s);
+	free(s);
+}
+
 int iTest = 1;
 int main(void)
 {
@@ -28,59 +36,72 @@ int main(void)
 	r = get_next_line(fd, &line);
 	/* 3 */ check(r == 0);
 	/* 4 */ check(line != (char *)42);
-	/* 5 */ mcheck(line, 1); free(line); line = (char *)42;
+	/* 5 */ mcheck(line, 1); free(line); readAll(fd); line = (char *)42;
+
+	fd = open("files/nl", O_RDWR);
+	r = get_next_line(fd, &line);
+	/* 6 */ check(r == 1);
+	/* 7 */ check(!strcmp(line, ""));
+	/* 8 */ mcheck(line, 1); free(line); readAll(fd); 
+	r = get_next_line(fd, &line);
+	/* 9 */ check(r == 0);
+	/* 10 */ check(!strcmp(line, ""));
+ 	free(line); readAll(fd); line = (char *)42;
 
 	fd = open("files/41_no_nl", O_RDWR);
 	r = get_next_line(fd, &line);
-	/* 6 */ check(r == 0);
-	/* 7 */ check(!strcmp(line, "01234567890123456789012345678901234567890"));
-	/* 8 */ mcheck(line, 42); free(line); line = (char *)42;
+	/* 11 */ check(r == 0);
+	/* 12 */ check(!strcmp(line, "01234567890123456789012345678901234567890"));
+	/* 13 */ mcheck(line, 42); free(line); readAll(fd); line = (char *)42;
 
 	fd = open("files/41_with_nl", O_RDWR);
 	r = get_next_line(fd, &line);
-	/* 9 */ check(r == 1);
-	/* 10 */ check(!strcmp(line, "0123456789012345678901234567890123456789"));
-	/* 11 */ mcheck(line, 41); free(line); line = (char *)42;
+	/* 14 */ check(r == 1);
+	/* 15 */ check(!strcmp(line, "0123456789012345678901234567890123456789"));
+	/* 16 */ mcheck(line, 41); free(line); line = (char *)42;
 	char c = 0; read(fd, &c, 1);
 	if (BUFFER_SIZE == 1)
-		/* 12 */ check(c == '0');
+		/* 17 */ check(c == '0');
 	else
 		++iTest;
+	readAll(fd);
 
 	fd = open("files/42_no_nl", O_RDWR);
 	r = get_next_line(fd, &line);
-	/* 13 */ check(r == 0);
-	/* 14 */ check(!strcmp(line, "012345678901234567890123456789012345678901"));
-	/* 15 */ mcheck(line, 43); free(line); line = (char *)42;
+	/* 18 */ check(r == 0);
+	/* 19 */ check(!strcmp(line, "012345678901234567890123456789012345678901"));
+	/* 20 */ mcheck(line, 43); free(line); readAll(fd); line = (char *)42;
 
 	fd = open("files/42_with_nl", O_RDWR);
 	r = get_next_line(fd, &line);
-	/* 16 */ check(r == 1);
-	/* 17 */ check(!strcmp(line, "01234567890123456789012345678901234567890"));
-	/* 18 */ mcheck(line, 42); free(line); line = (char *)42;
+	/* 21 */ check(r == 1);
+	/* 22 */ check(!strcmp(line, "01234567890123456789012345678901234567890"));
+	/* 23 */ mcheck(line, 42); free(line); line = (char *)42;
 	c = 0; read(fd, &c, 1);
 	if (BUFFER_SIZE <= 42)
-		/* 19 */ check(c == '1');
+		/* 24 */ check(c == '1');
 	else
 		++iTest;
+	readAll(fd);
 
 	fd = open("files/43_no_nl", O_RDWR);
 	r = get_next_line(fd, &line);
-	/* 20 */ check(r == 0);
-	/* 21 */ check(!strcmp(line, "0123456789012345678901234567890123456789012"));
-	/* 22 */ mcheck(line, 44); free(line); line = (char *)42;
+	/* 25 */ check(r == 0);
+	/* 26 */ check(!strcmp(line, "0123456789012345678901234567890123456789012"));
+	/* 27 */ mcheck(line, 44); free(line); readAll(fd); line = (char *)42;
 
 	fd = open("files/43_with_nl", O_RDWR);
 	r = get_next_line(fd, &line);
-	/* 23 */ check(r == 1);
-	/* 24 */ check(!strcmp(line, "012345678901234567890123456789012345678901"));
-	/* 25 */ mcheck(line, 43); free(line); line = (char *)42;
+	/* 28 */ check(r == 1);
+	/* 29 */ check(!strcmp(line, "012345678901234567890123456789012345678901"));
+	/* 30 */ mcheck(line, 43); free(line); line = (char *)42;
 	c = 0; read(fd, &c, 1);
 	if (BUFFER_SIZE == 1)
-		/* 26 */ check(c == '2');
+		/* 31 */ check(c == '2');
 	else
 		++iTest;
+	readAll(fd);
 	cout << ENDL;
-	r = get_next_line(0, &line); free(line);
+	//r = get_next_line(0, &line); free(line); readAll(fd);
 	return (0);
 }
