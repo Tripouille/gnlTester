3aecf4f34b5640b77fa265406757039b7de4b950
Tripouille <jgambard@student.42lyon.fr>
2020-12-07 12:56:13 +0100

    rewriting tests

--- tests/mandatory.cpp
+++ tests/mandatory.cpp
@@ -14,8 +14,8 @@ extern "C"
 #include "sigsegv.hpp"
 #include "check.hpp"
 #include "gnl.hpp"
-//Don't do that in real life
-#define TEST(x) {test = fork(); if (test == 0) {x} else {usleep(TIMEOUT_US); if (waitpid(test, &status, WNOHANG) == 0) {kill(test, 9); cout << FG_RED << iTest << ".TIMEOUT " << ENDL;}}}
+//Don't do that at home
+#define TEST(x) {test = fork(); if (test == 0) {x exit(EXIT_SUCCESS);} else {usleep(TIMEOUT_US); if (waitpid(test, &status, WNOHANG) == 0) {kill(test, 9); cout << FG_RED << "TIMEOUT";}}}
 
 int iTest = 1;
 int main(void)
@@ -24,34 +24,53 @@ int main(void)
 	cout << FG_LGRAY << "[BUFFER_SIZE = " << BUFFER_SIZE << "]: " << ENDL;
 	cout << FG_LGRAY << "Invalid fd: "; cout.flush();
 	TEST(/* 1 */ gnl(1000, -1, NULL);
-		 /* 2 */ gnl(-1, -1, NULL); cout << ENDL; iTest = 1;)
+		 /* 2 */ gnl(-1, -1, NULL);) cout << ENDL;
 
 	cout << FG_LGRAY << "files/empty: "; cout.flush();
 	fd = open("files/empty", O_RDWR);
 	TEST(/* 1 */ gnl(fd, 0, "");
-		 /* 2 */ gnl(fd, 0, ""); cout << ENDL; iTest = 1;)
+		 /* 2 */ gnl(fd, 0, "");) cout << ENDL; close(fd);
 
 	cout << FG_LGRAY << "files/nl: "; cout.flush();
 	fd = open("files/nl", O_RDWR);
 	TEST(/* 1 */ gnl(fd, 1, "");
-		 /* 2 */ gnl(fd, 0, ""); cout << ENDL; iTest = 1; close(fd);)
+		 /* 2 */ gnl(fd, 0, "");) cout << ENDL; close(fd);
 
 	cout << FG_LGRAY << "files/41_no_nl: "; cout.flush();
 	fd = open("files/41_no_nl", O_RDWR);
 	TEST(/* 1 */ gnl(fd, 0, "01234567890123456789012345678901234567890");
-		 /* 2 */ gnl(fd, 0, ""); cout << ENDL; iTest = 1; close(fd);)
+		 /* 2 */ gnl(fd, 0, "");) cout << ENDL; close(fd);
 
 	cout << FG_LGRAY << "files/41_with_nl: "; cout.flush();
 	fd = open("files/41_with_nl", O_RDWR);
 	TEST(/* 1 */ gnl(fd, 1, "0123456789012345678901234567890123456789");
 		 /* 2 */ gnl(fd, 0, "0"); 
-		 /* 2 */ gnl(fd, 0, ""); cout << ENDL; iTest = 1; close(fd);)
+		 /* 3 */ gnl(fd, 0, "");) cout << ENDL; close(fd);
+
+	cout << FG_LGRAY << "files/42_no_nl: "; cout.flush();
+	fd = open("files/42_no_nl", O_RDWR);
+	TEST(/* 1 */ gnl(fd, 0, "012345678901234567890123456789012345678901");
+		 /* 2 */ gnl(fd, 0, "");) cout << ENDL; close(fd);
 
 	cout << FG_LGRAY << "files/42_with_nl: "; cout.flush();
 	fd = open("files/42_with_nl", O_RDWR);
-	TEST(/* 1 */ gnl(fd, 1, "01234567890123456789012345678901234567890");
-		 /* 2 */ gnl(fd, 0, "1"); 
-		 /* 2 */ gnl(fd, 0, ""); cout << ENDL; iTest = 1; close(fd);)
+	TEST(/* 1 */ gnl(fd, 1, "01234567890123456789012345678901234567890"););
+	if (BUFFER_SIZE == 42)
+		 /* 2 */ {char c = 0; read(fd, &c, 1); ++iTest; check(c == '1'); --iTest;}
+	cout << ENDL; close(fd);
+
+	cout << FG_LGRAY << "files/43_no_nl: "; cout.flush();
+	fd = open("files/43_no_nl", O_RDWR);
+	TEST(/* 1 */ gnl(fd, 0, "0123456789012345678901234567890123456789012");
+		 /* 2 */ gnl(fd, 0, "");) cout << ENDL; close(fd);
+
+	cout << FG_LGRAY << "files/43_with_nl: "; cout.flush();
+	fd = open("files/43_with_nl", O_RDWR);
+	TEST(/* 1 */ gnl(fd, 1, "012345678901234567890123456789012345678901");
+		 /* 2 */ gnl(fd, 1, "2");
+		 /* 3 */ gnl(fd, 0, "");
+		 /* 4 */ gnl(fd, 0, "");) cout << ENDL; close(fd);
+
 
 	return (0);
 }
--- utils/check.cpp
+++ utils/check.cpp
@@ -5,9 +5,9 @@ extern int iTest;
 void check(bool succes)
 {
 	if (succes)
-		cout << FG_GREEN << iTest << ".OK ";
+		{cout << FG_GREEN << iTest << ".OK "; cout.flush();}
 	else
-		cout << FG_RED << iTest << ".KO ";
+		{cout << FG_RED << iTest << ".KO "; cout.flush();}
 }
 
 void mcheck(void * p, size_t required_size)
@@ -15,17 +15,17 @@ void mcheck(void * p, size_t required_size)
 	#ifdef __unix__
 	void * p2 = malloc(required_size); 
 	if (malloc_usable_size(p) == malloc_usable_size(p2))
-		cout << FG_GREEN << iTest << ".MOK ";
+		{cout << FG_GREEN << iTest++ << ".MOK "; cout.flush();}
 	else
-		cout << FG_RED << iTest << ".MKO ";
+		{cout << FG_RED << iTest++ << ".MKO "; cout.flush();}
 	free(p2);
 	#endif
 	#ifdef __APPLE__
 	void * p2 = malloc(required_size); 
 	if (malloc_size(p) == malloc_size(p2))
-		cout << FG_GREEN << iTest << ".MOK ";
+		{cout << FG_GREEN << iTest++ << ".MOK "; cout.flush();}
 	else
-		cout << FG_RED << iTest << ".MKO ";
+		{cout << FG_RED << iTest++ << ".MKO "; cout.flush();}
 	free(p2);
 	#endif
 }
