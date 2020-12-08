da60d8c632f4726dc0a002f5a5159b00251d1f76
Tripouille <jmichel.gambard@gmail.com>
2020-12-08 12:44:54 +0100

    remove useless structure

--- utils/gnl.hpp
+++ utils/gnl.hpp
@@ -3,12 +3,6 @@
 
 //Don't do that at home
 #define TEST(x) {int status = 0; int test = fork(); if (test == 0) {x exit(EXIT_SUCCESS);} else {usleep(TIMEOUT_US); if (waitpid(test, &status, WNOHANG) == 0) {kill(test, 9); cout << FG_RED << "TIMEOUT";}}}
-struct gnlInfos
-{
-	int fd, r;
-	char * s;
-	gnlInfos(int _fd, int _r, char * _s) : fd(_fd), r(_r), s(_s) {}
-};
 
 void gnl(int fd, int r, char * s);
 
