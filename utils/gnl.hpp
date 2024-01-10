803d2c8bb9e27e4410c4016dfece3aa38fe0402e
Paul Salame <psalame@2B5.42angouleme.fr>
2024-01-10 13:00:27 +0100

    optimise unessecary usleep when test ended before end of timeout

--- utils/gnl.hpp
+++ utils/gnl.hpp
@@ -1,9 +1,30 @@
 #ifndef GNL_HPP
 # define GNL_HPP
 # include "leaks.hpp"
+# include <ctime>
 
 //Don't do that at home
-#define TEST(x) {int status = 0; int test = fork(); if (test == 0) {x showLeaks(); exit(EXIT_SUCCESS);} else {usleep(TIMEOUT_US); if (waitpid(test, &status, WNOHANG) == 0) {kill(test, 9); cout << FG_RED << "TIMEOUT";}}}
+#define TEST(x) { \
+	int status = 0; \
+	int test = fork(); \
+	if (test == 0) { \
+		x showLeaks(); \
+		exit(EXIT_SUCCESS); \
+	} else { \
+		int nb_sleep = 0; \
+		const int usleep_time = TIMEOUT_US / 100; \
+		while (waitpid(test, &status, WNOHANG) == 0 && nb_sleep * usleep_time <= TIMEOUT_US) \
+		{ \
+			usleep(usleep_time); \
+			nb_sleep++; \
+		} \
+		if (waitpid(test, &status, WNOHANG) == 0) \
+		{ \
+			kill(test, 9); \
+			cout << FG_RED << "TIMEOUT"; \
+		} \
+	} \
+}
 
 void gnl(int fd, char const * s);
 
